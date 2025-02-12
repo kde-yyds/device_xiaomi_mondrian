9f40af05b2e93c65a6adc0b6563581f39abe29de
Michael Bestas <mkbestas@gmail.com>
2025-02-12 16:18:31 +0800

    Disable OMX service

--- device.mk
+++ device.mk
@@ -133,6 +133,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+TARGET_SUPPORTS_OMX_SERVICE := false
+
 # Automotive
 PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@2.0-manager-lib
